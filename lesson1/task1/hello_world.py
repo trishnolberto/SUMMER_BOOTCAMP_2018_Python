d81bc9046ccd485296234ebbe797db1671c04bca
Franz Lopez <franzelunico@gmail.com>
2018-01-30 15:45:08 -0400

    hello_world comentarios

--- lesson1/task1/hello_world.py
+++ lesson1/task1/hello_world.py
@@ -1,1 +1,6 @@
+"""
+Tradicionalmente ó, el primer programa que escribe en cualquier lenguaje de programacion es "Hello World!".
+Presentese al mundo.
+Ejecutar el script
+"""
 print("Welcome to Bootcamp 2018")
