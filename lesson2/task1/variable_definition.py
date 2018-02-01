97e6f75416cb6c5bc01fb164ad7009775bad15cc
trishnolberto <patricianolberto@hotmail.com>
2018-02-01 15:39:27 -0400

    PBN L2T1

---
+++ lesson2/task1/variable_definition.py
@@ -0,0 +1,13 @@
+"""
+Los comentarios en python comienzan con el caracter (#) e incluyen la linea.
+Agregue un nuevo comentario al archivo
+"""
+
+a = b = 2 #Esto se llama una asignacion encadenada". Asigna el valor 2 a las variables a y b
+print("a = " + str(a))  #Explicaremos la expresion str(a) mas adelante en el curso
+print("b = " + str(b))
+
+saludos = "saludos" #greetings
+print("saludos = " + str(saludos))
+saludos = 3
+print("saludos = " + str(saludos))
