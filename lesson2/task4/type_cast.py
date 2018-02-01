bbf8f579492e9a9bddb7344d66f37b2d00f81f13
trishnolberto <patricianolberto@hotmail.com>
2018-02-01 16:20:58 -0400

    PBN L2T4 cast

---
+++ lesson2/task4/type_cast.py
@@ -0,0 +1,16 @@
+"""
+Hay varias funciones integradas que le permiten convertir un tipo de datos a otro.
+Estas funciones devuelven un objeto que representa el valor convertido.
+int(x) convierte x en un entero.
+float(x) convierte x en un numero de coma flotante.
+str(x) convierte el objeto x en una representacion de cadena.
+
+Convierte float_number en entero.
+"""
+
+number = 9
+print(type(number))       #imprime el tipo de variable "number"
+
+float_number = 9.0
+print(float_number)
+print(int(float_number))
