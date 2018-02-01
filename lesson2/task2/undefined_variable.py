0ba7b9c5a2ed411ebb03296213dc95a560def979
trishnolberto <patricianolberto@hotmail.com>
2018-02-01 15:51:55 -0400

    PBN L2T2 probando con git

---
+++ lesson2/task2/undefined_variable.py
@@ -0,0 +1,11 @@
+"""
+
+Los nombres de las variables solo pueden contener letra,
+digitos o el caracter del subrayado, y no pueden comenzar con un digito.
+
+Compruebe lo que sucede si utiliza variable que aun no esta definida.
+Intenta imprimir un nombre indefinido.
+"""
+
+variable = 1
+print(variable)
