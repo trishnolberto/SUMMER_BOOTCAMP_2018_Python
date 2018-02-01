51991516ff3fd63a5b0fdcaebdbbec0bb80c76ae
trishnolberto <patricianolberto@hotmail.com>
2018-02-01 16:03:31 -0400

    PBN L2T3 type

---
+++ lesson2/task3/variable_type.py
@@ -0,0 +1,12 @@
+"""
+En python, hay dos tipos principales de numeros: enteros y flotante.
+La diferencia mas importante entre ellos es que un flotante es un numero que tiene un . y un int es un numero sin un punto decimal.
+
+Determine el tipo de variable float_number.
+"""
+
+number = 9
+print(type(number))       #imprime el tipo de variable "number"
+
+float_number = 9.0
+print(type(float_number))
