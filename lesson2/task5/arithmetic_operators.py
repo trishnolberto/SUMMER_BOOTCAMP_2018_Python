ee901dc27fb0ce748b6c2e9fd26654116b44b1e8
trishnolberto <patricianolberto@hotmail.com>
2018-02-03 14:14:14 -0400

    PBN L2T5

---
+++ lesson2/task5/arithmetic_operators.py
@@ -0,0 +1,16 @@
+"""
+Al igual que con cualquier otro lenguaje de programación, los operadores de
+suma (+),
+resta (-),
+multiplicación (*)
+división (/) se pueden usar con números.
+Además, Python tiene los operadores de potencia (**) y módulo (%).
+"""
+number = 9.0            # (float|int) number
+
+result = number/2     # (float)
+
+remainder = number%2  # (float|int)
+
+print("result = " + str(result))
+print("remainder = " + str(remainder))
