87c7d1e2f734a42a33b692f0b58faa7fdb3f06b1
trishnolberto <patricianolberto@hotmail.com>
2018-02-03 15:04:50 -0400

    PBN L3T8

---
+++ lesson3/task8/character_escaping.py
@@ -0,0 +1,10 @@
+"""
+La barra inclinada invertida se usa para escapar de comillas simples o dobles,
+por ejemplo 'It\'s me' o ' She said \'Hello\''.
+El símbolo especial '\n' se usa para agregar un salto de línea a una cadena.
+Las comillas simples se pueden usar en cadenas de comillas dobles sin escaparse y viceversa.
+"""
+dont_worry = "Don't worry about apostrophes"
+print(dont_worry)
+print("\"Sweet\" is an ice-cream")
+print('The name of this ice-cream is "Sweet\'n\'Tasty"')
