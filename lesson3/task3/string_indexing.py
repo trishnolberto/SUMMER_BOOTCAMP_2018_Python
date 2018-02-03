d9de7709e85550f0c107bce6b1742101949c8830
trishnolberto <patricianolberto@hotmail.com>
2018-02-03 14:56:34 -0400

    PBN L3T3

---
+++ lesson3/task3/string_indexing.py
@@ -0,0 +1,16 @@
+"""
+Puede acceder a un carácter de una cadena si conoce su posición.
+Por ejemplo, str[índice] producirá el carácter en el índice de posición
+en la cadena str.
+Tenga en cuenta que el índice de cadena siempre comienza en 0.
+Los índices también pueden ser números negativos,
+para comenzar a contar desde la derecha.
+Tenga en cuenta que -0 es lo mismo que 0,
+los índices negativos comienzan desde -1.
+"""
+
+python = "Python"
+print("h " + python[3])
+
+t_letter = python[2]
+print(t_letter)
