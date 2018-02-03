70e6491e7181067445ce91c53bf5f823ffb5dfbe
trishnolberto <patricianolberto@hotmail.com>
2018-02-03 14:43:13 -0400

    PBN L2T8

---
+++ lesson2/task8/comparison_operators.py
@@ -0,0 +1,18 @@
+"""
+Python tiene muchos tipos de operadores de comparación, incluidos
+>=,
+<=,
+>,
+<, etc.
+Todas las operaciones de comparación en Python tienen la misma prioridad.
+Las comparaciones arrojan valores booleanos: True o False.
+Las comparaciones pueden encadenarse arbitrariamente.
+"""
+one = 1
+two = 2
+three = 3
+
+print(one < two < three)
+
+is_greater = two > three
+print(is_greater)
