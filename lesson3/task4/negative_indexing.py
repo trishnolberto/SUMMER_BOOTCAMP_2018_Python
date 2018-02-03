a167d4709f435e1daf562c1782c635ee20014fed
trishnolberto <patricianolberto@hotmail.com>
2018-02-03 14:57:36 -0400

    PBN L3T4

---
+++ lesson3/task4/negative_indexing.py
@@ -0,0 +1,8 @@
+"""
+Puede usar números negativos en el operador de indexación
+para contar los caracteres 'hacia atrás' desde el final de la cadena.
+Use un índice negativo para obtener el '!' signo de long_string
+"""
+long_string = "This is a very but very long string!"
+exclamation = long_string[-35] # -27
+print(exclamation)
