fe81ff8caeff590e67e4925bfb68bc73fbb39951
trishnolberto <patricianolberto@hotmail.com>
2018-02-03 14:40:36 -0400

    PBN L2T7

---
+++ lesson2/task7/boolean_operators.py
@@ -0,0 +1,11 @@
+"""
+Boolean es un tipo de valor que solo puede ser verdadero o falso.
+El operador == (igualdad) verifica si las dos variables que se comparan
+son iguales.
+"""
+two = 2
+three = 4
+
+is_equal = two == three
+
+print(is_equal)
