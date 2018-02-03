25dbce04289a0dac2194266644cd20fcb4e8cd02
trishnolberto <patricianolberto@hotmail.com>
2018-02-03 14:49:43 -0400

    PBN L3T1 concatenation

---
+++ lesson3/task1/concatenation.py
@@ -0,0 +1,11 @@
+"""
+La combinación de dos cadenas usando el símbolo + se llama concatenación.
+"""
+
+hello = "Hola"
+space = "  "
+world = 'Mundo'
+
+
+hello_world = hello + space + world
+print(hello_world)
