143ff0afea47161f5d4ce93dd084f1830afa3aa5
trishnolberto <patricianolberto@hotmail.com>
2018-02-05 15:53:12 -0400

    PBN L4T6

---
+++ lesson4/task6/dict_key_value.py
@@ -0,0 +1,14 @@
+"""
+Hay muchos métodos útiles en diccionarios como keys() y values()
+"""
+
+phone_book = {"John": 123, "Jane": 234, "Jerard": 345}
+print(phone_book)
+
+
+phone_book["Jill"] = 456
+print(phone_book)
+
+print(phone_book.keys())
+
+print(phone_book.values())
