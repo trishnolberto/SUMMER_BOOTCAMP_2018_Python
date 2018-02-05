7e216bc4e16896c46494b43db5364bd4cd053563
trishnolberto <patricianolberto@hotmail.com>
2018-02-05 15:52:21 -0400

    PBN L4T4

---
+++ lesson4/task4/tuples.py
@@ -0,0 +1,16 @@
+"""
+Las tuplas son casi idénticas a las listas.
+La única diferencia significativa entre tuplas y listas es que
+las tuplas no se pueden cambiar
+no se pueden agregar,
+cambiar o eliminar elementos de la tupla.
+Las tuplas son construidas por un operador
+de coma encerrado entre paréntesis,
+por ejemplo (a, b, c).
+Una tupla de artículo solo articulo debe tener una coma final la coma
+como (d,)
+"""
+alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+
+print(alphabet)
