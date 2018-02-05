fc57905d2faa9733daa627d43577a434febb6813
trishnolberto <patricianolberto@hotmail.com>
2018-02-05 15:51:18 -0400

    PBN L4T2

---
+++ lesson4/task2/list_operations.py
@@ -0,0 +1,11 @@
+animals = ['elephant', 'lion', 'tiger', "giraffe"]
+print(animals)
+
+animals += ["monkey", 'dog']
+print(animals)
+
+animals.append("dino")
+print(animals)
+
+animals[6] = "dinosaur"
+print(animals)
