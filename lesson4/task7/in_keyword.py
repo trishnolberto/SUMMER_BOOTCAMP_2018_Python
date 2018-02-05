994d606161100e7ddd90108906fd573b82ed202c
trishnolberto <patricianolberto@hotmail.com>
2018-02-05 15:53:39 -0400

    PBN L4T7

---
+++ lesson4/task7/in_keyword.py
@@ -0,0 +1,13 @@
+"""
+La palabra clave "in" se usa para verificar si una lista o un diccionario
+contiene un elemento específico.
+Se puede aplicar a listas o diccionarios
+de la misma manera que se hizo con las cadenas.
+"""
+grocery_list = ["fish", "tomato", 'apples']
+
+print("tomato" in grocery_list)
+
+grocery_dict = {"fish": 1, "tomato": 6, 'apples': 3}
+
+print('fish' in grocery_dict)
